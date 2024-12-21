355ee56e9a20228e2c8886ce0c9256e71a1e6a60
devinfritz <devinfritz@gmx.de>
2024-12-21 11:23:21 +0100

    Add Author routing

--- src/main/kotlin/org/vzbot/plugins/routing/AuthorRouting.kt
+++ src/main/kotlin/org/vzbot/plugins/routing/AuthorRouting.kt
@@ -4,6 +4,7 @@ import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.dao.with
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.vzbot.models.BlogAuthor
 import org.vzbot.models.generated.toModel
@@ -11,8 +12,13 @@ import org.vzbot.models.generated.toModel
 fun Route.authors() {
     get("/author/{id}") {
         val authorID = call.request.pathVariables["id"]?.toIntOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest)
-        val author = transaction { runBlocking {  BlogAuthor.findById(authorID)?.toModel()?.with { posts() } } } ?: return@get call.respond(HttpStatusCode.NotFound)
+        val author = transaction { runBlocking { BlogAuthor.findById(authorID)?.toModel()?.with { posts() } } } ?: return@get call.respond(HttpStatusCode.NotFound)
 
         call.respond(author)
+    }
+
+    get("/authors") {
+        val authors = transaction { BlogAuthor.all().map { it.toModel() } }
+        call.respond(authors)
     }
 }
